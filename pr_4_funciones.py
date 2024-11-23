0c158374b43ceda84c087463274248f1b50b26ed
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-11-23 01:30:03 -0600

    Add files via upload

---
+++ pr_4_funciones.py
@@ -0,0 +1,11 @@
+print("nolasco_aguilar_martha_sofia_0948_3W")
+def es_vocal(caracter):#abre una funcion
+    
+    return caracter.lower() in 'aeiou'#regresa a verdad si son vocales 
+
+
+print(es_vocal('a'))  # regresa a verdad si son vocales 
+print(es_vocal('E'))   # regresa a verdad si son vocales 
+print(es_vocal('z'))  # indica falso si no son vocales
+print(es_vocal('1'))  # indica falso si no son vocales
+ 
